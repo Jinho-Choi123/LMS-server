f40b12a0893a2442bd463577485c1ca864676d55
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-26 10:25:42 +0000

    fix:delete unused middleware

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 const loginMiddleware = require('../middlewares/auth/loginMiddleware');
 const registerMiddleware = require('../middlewares/auth/registerMiddleware');
-const checkMiddleware = require('../middlewares/auth/checkMiddleware');
+//const checkMiddleware = require('../middlewares/auth/checkMiddleware');
 
 router.post('/login', loginMiddleware);
 
 router.post('/register', registerMiddleware);
 
-router.post('/check', checkMiddleware);
+//router.post('/check', checkMiddleware);
 
 module.exports = router;
