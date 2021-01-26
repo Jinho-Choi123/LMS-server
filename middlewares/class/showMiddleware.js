191bcec89fe71cb292663793ab140e6066fe0f34
inhwa <igreen0485@gmail.com>
2021-01-26 09:55:22 +0000

    feature:completed getting classes for students

--- middlewares/class/showMiddleware.js
+++ middlewares/class/showMiddleware.js
@@ -8,44 +8,118 @@ const showMiddleware = (req, res, next) => {
     const isStudent = req.body.isStudent;
     const userId = req.body.userId
 
-    User.findOne({ userId: userId }, (err, data) => {
-        console.log(data)
-            if (err) throw err;
-            if (isStudent) {
-                const classes = data.lectureIn
-                const classesInfo = []
-                classes.forEach(element =>{
-                    console.log(element)
-                    var profId;
-                    var className;
-                    Class.findOne({classId:element},function(err,data){
-                        if(err) throw err;
-                        else{
-                            profId = data.instructor
-                            className = data.className
-                            console.log(className)
-                            classesInfo.push({className:data.className, instructor:profId, classId:element})
-                            console.log(classesInfo)
-                            res.json({ classes: classesInfo, msg: 'success', success: true })
-                        }
-                    })
-                })
-            } else {
-                res.json({
-                    msg: err.message,
-                    success: false
-                })
-            }
-
+    const resolve = (classesInfo) =>{
+        return new Promise((resolve,reject) =>{
+            resolve(classesInfo);
         })
-        .catch((err) => {
-            res.json({
-                msg: err.message,
-                success: false
+    }
+
+    const findClass = (classes) =>{
+        return new Promise((resolve, reject) =>{
+            var classesInfo = [];
+            classes.forEach(element =>{
+                console.log(element)
+                var profId;
+                var className;
+                Class.findOne({classId:element},function(err,data){
+                    if(err) throw err;
+                    else{
+                        profId = data.instructor
+                        className = data.className
+                        console.log(className)
+                        classesInfo.push({className:data.className, instructor:profId})
+                        console.log("1",classesInfo)
+                    }
+                })
+            })
+            setTimeout(()=>{resolve(classesInfo)},1000)
+            console.log("2",classesInfo)
+        })
+    }
+
+    const findUser = (userId) =>{
+        return new Promise((resolve, reject) =>{
+            User.findOne({ userId: userId }, (err, data) => {
+                if (err) throw err;
+                const classes = data.lectureIn
+                resolve(classes)
             })
         })
+    }
+
+    findUser(userId)
+    .then(classes => findClass(classes))
+    .then(classesInfo=>res.json({ classes: classesInfo, msg: 'success', success: true }))
+    .catch(err => console.log("err",err))
 
 
 }
+
+
+// const showMiddleware = (req, res, next) => {
+//     //console.log("req", req.body)
+//     const isStudent = req.body.isStudent;
+//     const userId = req.body.userId
+
+//     var i = 0;
+//     //const delay = ms =>new Promise(resolve=>setTimeout(resolve, ms))
+
+//     async function findClassforStudent(classes){
+//         return new Promise((resolve, reject) =>{
+//             const classesInfo = []
+//             function classesforEach(classes, classesInfo){
+//                 classes.forEach(element =>{
+//                     i++;
+//                     console.log(element)
+//                     Class.findOne({classId:element},function(err,data){
+//                         if(err) throw err;
+//                         else{
+//                             var profId = data.instructor
+//                             var className = data.className
+//                             console.log("1 classname",className)
+//                             classesInfo.push({className:data.className, instructor:profId, classId:element})
+//                             console.log("2 classinfo",classesInfo)     
+//                             //resolve(classesInfo)                   
+//                         }
+//                     })
+//                     if(i==classes.length) {
+//                         console.log("3 i",i)
+//                         console.log("4 classesInfo at last",classesInfo)
+//                         //resolve(classesInfo)
+//                     }
+//                 })
+//             }
+//             classesforEach(classes,classesInfo)
+//             //await delay(1000)
+//             resolve(classesInfo)
+//             //setTimeout(()=>{resolve(classesInfo)} , 1000)
+            
+//             //await resolve(classesInfo)
+//             //console.log("classInfo outside",classesInfo)
+//             //resolve(classesInfo)
+//         })
+//     }
+//     function findUser(userId){
+//         return new Promise((resolve, reject)=>{
+//             User.findOne({userId:userId},(err,data)=>{
+//                 if(err) throw err;
+//                 //const classes = data.lectureIn;
+//                 //console.log("lectureIn",data.lectureIn)
+//                 resolve(data.lectureIn);
+//             })
+//         })
+//     }
+//     findUser(userId)
+//     .then(classes => findClassforStudent(classes))
+//     .then(classesInfo => res.json({ classes: classesInfo, msg: 'success', success: true }))
+//     .catch((err) => {
+//         res.json({
+//             msg: err.message,
+//             success: false
+//         })
+//     })
+   
+    
+// }
 
 module.exports = showMiddleware;
